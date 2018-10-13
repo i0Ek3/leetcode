9f215b407b99dbbb253d2ba94267ed61fab50c42
i0Ek3 <kno30826@gmail.com>
2018-10-13 16:47:45 +0800

    Update twoSum.cpp

--- C++/twoSum/twoSum.cpp
+++ C++/twoSum/twoSum.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello world" << std::endl;
-    return 0;
-}
-
+class Solution {
+public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+                unordered_map<int, int> m;
+                vector<int> result;
+                for (int i = 0; i < nums.size(); i++) {
+                        m[nums[i]] = i;
+                }
+                for (int i = 0; i < nums.size(); i++) {
+                        int value = target - nums[i];
+                        if (m.count(value) && m[value] != i) {
+                                result.push_back(i);
+                                result.push_back(m[value]);
+                                break;
+                        }
+                }
+                return result;
+        }
+};
