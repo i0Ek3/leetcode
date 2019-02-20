f8a18c2e47c6422ede03f6fae49d01e39ee3fd27
i0Ek3 <kno30826@gmail.com>
2019-02-20 10:43:48 +0800

    Refactor 020.cpp

--- C++/020.cpp
+++ C++/020.cpp
@@ -3,9 +3,14 @@ public:
     bool isValid(string s) {
         stack<char> st;
         for (auto c : s) {
-            if (!st.empty()) return true;
-            if ((c == ')' && st.top() == '(') || (c == ']' && st.top() == '[') || (c == '}' && st.top() == '{')) st.pop();
-            else st.push(c);
+            if (c == '(' || c == '[' || c == '{') st.push(c);
+            else {
+                if (!st.empty()) return true;
+                if (c == ')' && st.top() != '(') return false;
+                if (c == '}' && st.top() != '{') return false;
+                if (c == ']' && st.top() != '[') return false;
+                st.pop();
+            }
         }
         return st.empty(); // 空则完全匹配了，非空则不匹配
     }
