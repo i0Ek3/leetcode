2afbffb832f74de2f8f39f8f0cd3b45e06f3beba
i0Ek3 <kno30826@gmail.com>
2019-04-26 15:36:30 +0800

    Add lock problems description

---
+++ C++/251.cpp
@@ -0,0 +1,17 @@
+// 251 Flatten 2D Vector
+//
+// Implement an iterator to flatten a 2d vector.
+//
+// Example:
+//
+// Input: 2d vector =
+// [
+//   [1,2],
+//     [3],
+//       [4,5,6]
+//       ]
+//       Output: [1,2,3,4,5,6]
+//       Explanation: By calling next repeatedly until hasNext returns false, 
+//                    the order of elements returned by next should be: [1,2,3,4,5,6].
+// ]
+//
---
+++ C++/269.cpp
@@ -0,0 +1,18 @@
+// 269 Alien Dictionary
+// 
+// There is a new alien language which uses the latin alphabet. However, the order among letters are unknown to you. You receive a list of non-empty words from the dictionary, where words are sorted lexicographically by the rules of this new language. Derive the order of letters in this language.
+//
+// Example 1:
+//
+// Input:
+// [
+//   "wrt",
+//     "wrf",
+//       "er",
+//         "ett",
+//           "rftt"
+//           ]
+//
+//           Output: "wertf"
+// ]
+//
