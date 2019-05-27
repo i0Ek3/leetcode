1dcb6dc232a774ec7b031e7702d78a2776508ee3
f4k1c0de2 <kno30826@gmail.com>
2019-05-27 22:26:23 +0800

    Rewrite 001.cpp

--- C++/001.cpp
+++ C++/001.cpp
@@ -1,19 +1,11 @@
 class Solution {
 public:
-        vector<int> twoSum(vector<int>& nums, int target) {
-                unordered_map<int, int> m;
-                vector<int> result;
-                for (int i = 0; i < nums.size(); i++) {
-                        m[nums[i]] = i;
-                }
-                for (int i = 0; i < nums.size(); i++) {
-                        int value = target - nums[i];
-                        if (m.count(value) && m[value] != i) {
-                                result.push_back(i);
-                                result.push_back(m[value]);
-                                break;
-                        }
-                }
-                return result;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> res;
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = res.find(target - nums[i]);
+            if (it != res.end()) return {it->second, i};
         }
+        res[nums[i]] = i;
+    }
 };
